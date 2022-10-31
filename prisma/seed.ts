09af34aefc4332a5a18436d56d37c42b2480f1d7
Dom van Almsick <dominicvana@gmail.com>
2022-10-31 18:15:51 -0500

    chore: db seeded

---
+++ prisma/seed.ts
@@ -0,0 +1,54 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+
+const prisma = new PrismaClient();
+
+const seedProjects = [
+  {
+    name: 'SpaceX',
+    description:
+      'SpaceX designs, manufactures and launches advanced rockets and spacecraft.',
+  },
+  {
+    name: 'Tesla',
+    description:
+      'Tesla designs and manufactures electric vehicles and energy storage products.',
+  },
+  {
+    name: 'Neuralink',
+    description:
+      'Neuralink is developing ultra-high bandwidth brain-machine interfaces to connect humans and computers.',
+  },
+];
+
+async function main() {
+  const seedUser = await prisma.user.create({
+    data: {
+      username: 'elon',
+      passwordHash: bcrypt.hashSync('musk', 10),
+    },
+  });
+
+  console.log('New user created:', seedUser);
+
+  for (const { name, description } of seedProjects) {
+    const project = await prisma.project.upsert({
+      where: { name },
+      create: { name, description, userId: seedUser.id },
+      update: { name, description, userId: seedUser.id },
+    });
+
+    console.log('Newly created project:', project);
+  }
+}
+
+main()
+  .then(async () => {
+    console.log('Database seeded successfully');
+    await prisma.$disconnect;
+  })
+  .catch(async e => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
