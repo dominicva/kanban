401cf4ff4751d114c74b7a9ca3d06411674b9e15
Dom van Almsick <dominicvana@gmail.com>
2022-11-02 03:04:50 -0500

    feat: init custom theming

--- app/theme.ts
+++ app/theme.ts
@@ -1,11 +1,65 @@
-import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
+import type { ThemeConfig } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+import { defineStyle, defineStyleConfig } from '@chakra-ui/styled-system';
 
-const config: ThemeConfig = {
-  initialColorMode: 'dark',
-  useSystemColorMode: true,
-};
+const baseStyle = defineStyle({
+  borderRadius: '20px',
+  fontWeight: 'bold',
+  fontFamily: 'Plus Jakarta Sans',
+  fontSize: '13px',
+  lineHeight: '23px',
+});
 
-// 3. extend the theme
-const theme = extendTheme({ config });
+const customVariant = defineStyle(props => {
+  const { colorScheme: c } = props;
+  return {
+    bg: `${c}.500`,
+    color: 'white',
+    borderRadius: '20px',
+    fontWeight: 'bold',
+    fontFamily: 'Plus Jakarta Sans',
+    fontSize: '13px',
+    lineHeight: '23px',
+    transition: 'transform 0.15s ease-out, backgroun 0.15s ease-out',
+    _dark: {
+      bg: `${c}.200`,
+      color: 'gray.900',
+    },
+    _hover: {
+      bg: `${c}.600`,
+      transform: 'scale(1.05)',
+      _dark: {
+        bg: `${c}.300`,
+      },
+    },
+    _active: {
+      bg: `${c}.700`,
+      transform: 'scale(1, 1)',
+      _dark: {
+        bg: `${c}.400`,
+      },
+    },
+  };
+});
+
+export const buttonTheme = defineStyleConfig({
+  baseStyle,
+  variants: {
+    custom: customVariant,
+  },
+  defaultProps: {
+    colorScheme: 'purple',
+  },
+});
+
+const theme: ThemeConfig = extendTheme({
+  config: {
+    initialColorMode: 'dark',
+    useSystemColorMode: true,
+  },
+  components: {
+    Button: buttonTheme,
+  },
+});
 
 export default theme;
