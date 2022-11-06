f4d2997dce2d94df3373c636d421947b9bc96952
Dom van Almsick <dominicvana@gmail.com>
2022-11-06 06:52:07 -0500

    refactor: add column to own component

--- app/components/Column.tsx
+++ app/components/Column.tsx
@@ -10,28 +10,17 @@ export default function Column({
   tasks: Array<Task>;
 }) {
   return (
-    <GridItem
-      colSpan={1}
-      borderRadius="md"
-      border="1px"
-      borderColor="_gray.800"
-      boxShadow="md"
-      p={4}
-      minW="250px"
-      maxW="250px"
-      h="100%"
-    >
+    <GridItem borderRadius="md" boxShadow="md" p={4} w="280px" h="100%">
       <Flex justifyContent="space-between" alignItems="center">
         <Text
           style={{ fontVariant: 'small-caps' }}
           letterSpacing="2.4px"
           color="gray.400"
-          mb={5}
+          mb={6}
         >
           {title} ({tasks.length})
         </Text>
       </Flex>
-      <Box h="1px" bg="_gray.100" mt={2} mb={4} />
       <Box>
         {tasks.map(task => (
           <Box key={task.id} textStyle="h4" mb={4}>
--- app/routes/dashboard/__projects/$project/index.tsx
+++ app/routes/dashboard/__projects/$project/index.tsx
@@ -1,7 +1,7 @@
 import type { LoaderArgs } from '@remix-run/node';
 import type { Column } from '@prisma/client';
 import { json } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { Link, useLoaderData } from '@remix-run/react';
 import {
   Box,
   Button,
@@ -32,36 +32,51 @@ export const loader = async ({ request, params }: LoaderArgs) => {
   return json(columns);
 };
 
+function AddColumn() {
+  return (
+    <Flex
+      as={Link}
+      to="columns/new"
+      borderRadius="md"
+      border="transparent"
+      bg={useColorModeValue('_gray.100', '#22232E')}
+      boxShadow="md"
+      p={4}
+      w="250px"
+      h="100%"
+      alignItems={'center'}
+      justifyContent={'center'}
+      flexDir={'column'}
+      transition={'all 0.2s ease-out'}
+      _hover={{
+        bg: useColorModeValue('_gray.200', '#2A2C37'),
+      }}
+    >
+      <Button
+        variant={'ghost'}
+        _hover={{ bg: 'transparent' }}
+        leftIcon={<MdAdd />}
+        letterSpacing="2.4px"
+        color="gray.400"
+        mb={5}
+      >
+        New Column
+      </Button>
+    </Flex>
+  );
+}
+
 export default function ColumnsRoute() {
   const data = useLoaderData();
+
+  const haveColumns = data.columns.length > 0;
 
   return (
     <Flex h="100%" gap={6}>
       {data.columns.map((column: Column) => (
         <KanbanColumn key={column.id} title={column.title} tasks={[]} />
       ))}
-      <Flex
-        borderRadius="md"
-        border="transparent"
-        bg={useColorModeValue('_gray.800', '#22232E')}
-        boxShadow="md"
-        p={4}
-        w="250px"
-        h="100%"
-        alignItems={'center'}
-        justifyContent={'center'}
-        flexDir={'column'}
-      >
-        <Button
-          variant={'ghost'}
-          leftIcon={<MdAdd />}
-          letterSpacing="2.4px"
-          color="gray.400"
-          mb={5}
-        >
-          New Column
-        </Button>
-      </Flex>
+      {haveColumns ? <AddColumn /> : null}
     </Flex>
   );
 }
