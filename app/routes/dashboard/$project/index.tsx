8e8c86b03b5c3d6959162c20fcdc0b1cfa41dc8e
Dom van Almsick <dominicvana@gmail.com>
2022-11-07 19:04:42 -0500

    add column grid item

--- app/routes/dashboard/$project/index.tsx
+++ app/routes/dashboard/$project/index.tsx
@@ -1,11 +1,12 @@
 import type { LoaderArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import { Box, Flex, Grid, GridItem, Text } from '@chakra-ui/react';
-import { useLoaderData, useParams } from '@remix-run/react';
+import { Box, Button, Flex, Grid, GridItem, Text } from '@chakra-ui/react';
+import { Link, useLoaderData, useParams } from '@remix-run/react';
 import { getProject } from '~/models/project.server';
 import { getUserId } from '~/utils/session.server';
 import { db } from '~/utils/db.server';
 import Column from '~/components/Column';
+import { MdAdd } from 'react-icons/md';
 
 export const loader = async ({ request, params }: LoaderArgs) => {
   if (!params.project || typeof params.project !== 'string') {
@@ -44,7 +45,7 @@ export default function ProjectIndex() {
   console.log('project columns', project.columns);
   const haveColumns = project.columns && project.columns.length > 0;
   return (
-    <Grid templateColumns="repeat(auto-fit, 280px)" gap={6}>
+    <Grid templateColumns="repeat(auto-fit, 280px)" gap={6} h="100%">
       {haveColumns
         ? project.columns.map(column => {
             return (
@@ -70,6 +71,35 @@ export default function ProjectIndex() {
             );
           })
         : null}
+      {project.columns.length > 0 ? (
+        <GridItem
+          as={Link}
+          to="new?resource=column"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          textStyle="h2"
+          letterSpacing="2px"
+          bg="#22232E"
+          transition="all 0.2s"
+          color="_gray.400"
+          _hover={{ color: '_gray.100', transform: 'scale(1.05)' }}
+          borderRadius="md"
+          boxShadow="md"
+          p={4}
+          w="280px"
+          h="100%"
+        >
+          <Button
+            variant="ghost"
+            leftIcon={<MdAdd />}
+            color="inherit"
+            _hover={{ bg: 'transparent' }}
+          >
+            New Column
+          </Button>
+        </GridItem>
+      ) : null}
     </Grid>
   );
 }
